62a3a7cb7b2216d51d4c47978318cf73cc9fb841
andrew9078 <andrewjung98@gmail.com>
2024-03-11 17:58:14 -0700

    1st commit
    Foundational React infrastructure

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import SimpleComponent from './SimpleComponent'; 
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Simple React App</h1>
+          <SimpleComponent /> 
+      </div>
+  );
+}
+
+export default App;
---
+++ src/SimpleComponent.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+
+
+const SimpleComponent = () => {
+    const [count, setCount] = useState(0);
+
+    const incrementCount = () => {
+        setCount(count + 1);
+    };
+
+    return(
+        <div>
+            <p>Count: {count}</p>
+            <button onClick={incrementCount}>Increment</button>
+        </div>
+    );
+};
+
+export default SimpleComponent;
